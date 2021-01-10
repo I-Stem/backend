c209f9d968503f371e6e5dac5fc93083f403d384
Suman Kumar <skcool.123bgp@gmail.com>
2021-01-10 16:08:29 +0530

    add route authorization for staffs

--- src/middlewares/Auth.ts
+++ src/middlewares/Auth.ts
@@ -3,68 +3,74 @@
  *
  */
 
-import * as jwt from "jsonwebtoken";
-import Locals from "../providers/Locals";
-import { Request, Response, Application, NextFunction } from "express";
-import { createResponse, response } from "../utils/response";
-import * as HttpStatus from "http-status-codes";
-import loggerFactory from "./WinstonLogger";
-import { UniversityRoles } from "src/domain/UniversityModel";
+import * as jwt from 'jsonwebtoken';
+import Locals from '../providers/Locals';
+import { Request, Response, Application, NextFunction } from 'express';
+import { createResponse, response } from '../utils/response';
+import * as HttpStatus from 'http-status-codes';
+import loggerFactory from './WinstonLogger';
+import { UniversityRoles } from 'src/domain/UniversityModel';
 
 class Auth {
-    static servicename = "Auth";
+    static servicename = 'Auth';
 
     public static ACCESS_TOKEN_MISSING_ERROR_MESSAGE: string = `The request is missing access token`;
     public static ACCESS_TOKEN_EXPIRED_ERROR_MESSAGE: string = `The access token has expired`;
 
-    private static AUTH_NOT_REQUIRED_ENDPOINTS =
-        process.env.EXEMPT_ENDPOINT?.split(",") || [];
+    private static AUTH_NOT_REQUIRED_ENDPOINTS = [
+        '/auth',
+        'api/v1/ocr/callback',
+        'api/vc/callback',
+        '/service/email',
+        'api/university/organ/req',
+    ];
 
-    private static AUTHORIZED_ROUTES_FOR_STAFFS =
-        process.env.EXEMPT_ENDPOINT_FOR_STAFFS?.split(",") || [];
+    private static AUTHORIZED_ROUTES_FOR_STAFFS = [
+        'api/university/index',
+        'api/university/invite',
+        'api/university/settings',
+        'api/university/metrics',
+    ];
 
-    /**
-     * Function to verify authorization of API endpoint.
-     * Returns true if the endpoint is not accessible to user.
-     *
-     * @param path API endpoint
-     * @param role User Role
-     */
     private static verifyRouteAuthorization(
+        res: Response,
         path: string,
         role: UniversityRoles
-    ): boolean {
+    ) {
         if (
             Auth.AUTHORIZED_ROUTES_FOR_STAFFS.some(
                 (endpoint) =>
                     path.includes(endpoint) && role !== UniversityRoles.STAFF
             )
         ) {
-            return true;
+            return createResponse(
+                res,
+                HttpStatus.FORBIDDEN,
+                'Unauthorized user'
+            );
         }
-        return false;
     }
 
     public static verifyToken(req: Request, res: Response, next: NextFunction) {
-        let logger = loggerFactory(Auth.servicename, "verifyToken");
+        let logger = loggerFactory(Auth.servicename, 'verifyToken');
         if (
             Auth.AUTH_NOT_REQUIRED_ENDPOINTS.some((endpoint) =>
                 req.path.includes(endpoint)
             )
         ) {
             logger.info(
-                "Request authorization skipped for the requested endpoint " +
+                'Request authorization skipped for the requested endpoint ' +
                     req.path
             );
             return next();
         }
 
         const _token = Auth.getToken(req);
-        if (_token === "") {
-            logger.info("Request is missing access token");
+        if (_token === '') {
+            logger.info('Request is missing access token');
             return res.status(HttpStatus.UNAUTHORIZED).json(
                 response[HttpStatus.UNAUTHORIZED]({
-                    message: Auth.ACCESS_TOKEN_MISSING_ERROR_MESSAGE,
+                    message: Auth.ACCESS_TOKEN_MISSING_ERROR_MESSAGE
                 })
             );
         }
@@ -74,41 +80,34 @@ class Auth {
             function (err: any, decoded: any) {
                 if (err) {
                     switch (err.name) {
-                        case "TokenExpiredError":
-                            logger.error("Access token has expired");
+                        case 'TokenExpiredError':
+                            logger.error('Access token has expired');
                             return res.status(HttpStatus.UNAUTHORIZED).json(
                                 response[HttpStatus.UNAUTHORIZED]({
                                     message:
-                                        Auth.ACCESS_TOKEN_EXPIRED_ERROR_MESSAGE,
+                                        Auth.ACCESS_TOKEN_EXPIRED_ERROR_MESSAGE
                                 })
                             );
-                        case "JsonWebTokenError":
+                        case 'JsonWebTokenError':
                             logger.error(
-                                "Invalid json web token error: " + err.message
+                                'Invalid json web token error: ' + err.message
                             );
                             return res.status(HttpStatus.FORBIDDEN).json(
                                 response[HttpStatus.FORBIDDEN]({
-                                    message: err.message,
+                                    message: err.message
                                 })
                             );
                     }
                 }
                 res.locals.user = decoded; // If no error, token info is returned in 'decoded'
-                if (Auth.verifyRouteAuthorization(req.path, decoded.role)) {
-                    return createResponse(
-                        res,
-                        HttpStatus.FORBIDDEN,
-                        "Unauthorized user"
-                    );
-                } else {
-                    next();
-                }
+                Auth.verifyRouteAuthorization(res, req.path, decoded.role);
+                next();
             }
         );
     }
 
     public static getToken(req: Request): string {
-        let methodName = "getToken";
+        let methodName = 'getToken';
 
         let logger = loggerFactory(Auth.servicename, methodName);
         if (
