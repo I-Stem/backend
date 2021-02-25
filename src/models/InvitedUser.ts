9ede765c2efdc411573f12563fb64f858eb51af2
explorer <choudhary.linus@gmail.com>
2021-02-25 12:03:08 +0530

    signature change

--- src/models/InvitedUser.ts
+++ src/models/InvitedUser.ts
@@ -53,7 +53,7 @@ const InvitedUserSchema = new mongoose.Schema(
  *  Populate InvitedUser with verify Token
  */
 
-InvitedUserSchema.pre("insertMany",  ( docs) => {
+InvitedUserSchema.pre<InvitedUserModel[]>("insertMany",  function(this:InvitedUserModel[], next, docs) {
     docs.map((user: InvitedUserModel) => {
         const verifyToken = Buffer.from(
             (user.email + (Math.random() * 1000).toString).trim()
